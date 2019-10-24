e2e069f78bad29a42ccb9f159c7e32fff8c2cc4e
Philipp Piwo <github@piwo.space>
2019-10-24 12:48:03 +0200

    update for lima

--- src/App.vue
+++ src/App.vue
@@ -3,28 +3,21 @@
 </template>
 
 <script>
-  import Universal from '@aeternity/aepp-sdk/es/ae/universal'
   import Aepp from '@aeternity/aepp-sdk/es/ae/aepp'
   import identitySource from './util/Identity.aes'
 
   export default {
     name: 'app',
     async mounted () {
-      const client = await Aepp()
-      /*
-      const client = await Universal({
-         url: 'http://localhost:3001',
-         internalUrl: 'http://localhost:3001',
-         compilerUrl:  'http://localhost:3080'
-       });
-      */
-      const contract = await client.getContractInstance(identitySource)
-      await contract.deploy()
-      console.log(contract.deployInfo.address)
+      const client = await Aepp();
 
-      console.log('Test')
-      console.log(await contract.methods.main(0))
-      console.log(await client.getNodeInfo())
+      const contract = await client.getContractInstance(identitySource);
+      await contract.deploy();
+      console.log(contract.deployInfo.address);
+
+      console.log('Test');
+      console.log(await contract.methods.main(0));
+      console.log(await client.getNodeInfo());
     },
   }
 </script>
