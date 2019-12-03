f58c86b6800b28739134c6d7b409791b4c469130
Philipp Piwo <github@piwo.space>
2019-12-03 13:41:46 +0200

    add base-aepp version test

--- src/App.vue
+++ src/App.vue
@@ -9,7 +9,7 @@
   export default {
     name: 'app',
     async mounted () {
-      const client = await Aepp();
+      const client = await Aepp.compose({deepConfiguration: {Ae: {methods: ['baseAppVersion']}}})();
 
       const contract = await client.getContractInstance(identitySource);
       await contract.deploy();
@@ -18,6 +18,7 @@
       console.log('Test');
       console.log(await contract.methods.main(0));
       console.log(await client.getNodeInfo());
+      console.log(await client.baseAppVersion());
     },
   }
 </script>
